926162114b123e6759c4d45bf39ce5367a080e2d
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 11:46:21 +0900

    a

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
-from functionality import player, dialogue, Item
+import functionality
 
 st.title('Text-based Tabletop RPG')
 
 # Sidebar with tabs
 sidebar_tabs = st.sidebar.tabs(["Profile", "Settings"])
 with sidebar_tabs[0]:
-    st.image(player.image, caption="Player")
+    st.image(functionality.player.image, caption="Player")
     st.write("Stats")
-    for stat, value in player.stats.items():
+    for stat, value in functionality.player.stats.items():
         st.write(f"{stat}: {value}")
 
     st.write("Equipment")
-    for part, equipment in player.equipment.items():
+    for part, equipment in functionality.player.equipment.items():
         st.write(f"{part}: {equipment}")
 
     st.write("Inventory")
-    for item in player.inventory:
+    for item in functionality.player.inventory:
         st.image(item.image)
         st.write(f"Name: {item.name}")
         st.write(f"Modifier: {item.modifier}")
@@ -33,17 +33,17 @@ with main_tabs[0]:
     st.chat_input("Enter text here")
 
 with main_tabs[1]:
-    dialogue_tabs = st.tabs(list(dialogue.speakers.keys()))
-    for speaker in dialogue.speakers:
+    dialogue_tabs = st.tabs(list(functionality.dialogue.speakers.keys()))
+    for speaker in functionality.dialogue.speakers:
         with dialogue_tabs[speaker]:
-            st.image(dialogue.speakers[speaker]["image"], caption=speaker)
-            for text in dialogue.speakers[speaker]["dialogues"]:
+            st.image(functionality.dialogue.speakers[speaker]["image"], caption=speaker)
+            for text in functionality.dialogue.speakers[speaker]["dialogues"]:
                 st.write(text)
 
 # Debug buttons
 if st.button("Toggle Debug Buttons"):
     if st.button("Add Item"):
-        player.inventory.append(Item("placeholder_item_image.png", "Sword", "+10", "Attack", "A sharp sword."))
+        functionality.player.inventory.append(functionality.Item("placeholder_item_image.png", "Sword", "+10", "Attack", "A sharp sword."))
     if st.button("Add Dialogue"):
-        dialogue.add_speaker("NPC1", "placeholder_npc1_image.png")
-        dialogue.add_dialogue("NPC1", "Hello!")
+        functionality.dialogue.add_speaker("NPC1", "placeholder_npc1_image.png")
+        functionality.dialogue.add_dialogue("NPC1", "Hello!")
