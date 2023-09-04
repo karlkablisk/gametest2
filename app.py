d7fdebd42e189a8dcd26485d31afb92a2ed25dc1
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 16:44:21 +0900

    a

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ sidebar_tabs = st.sidebar.tabs(["Profile", "Settings", "Debug"])
 
 # Adjusted location image and caption
 location_image = functionality.get_image_or_placeholder('placeholder_location_image.png', 'yellow')
-st.image(location_image, caption=functionality.location_name, height=300, output_format="PNG")
+location_image = Image.open(location_image)
+location_image = location_image.resize((location_image.width, 300))
+st.image(location_image, caption=functionality.location_name, output_format="PNG")
+
 
 with sidebar_tabs[0]:
     st.image(functionality.player.image, caption=functionality.player.name)  # Updated caption to dynamic player name
