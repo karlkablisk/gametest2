bc4790998f72b5c640a565c44ffcef198c80514d
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 12:02:50 +0900

    a

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ with sidebar_tabs[1]:
 main_tabs = st.tabs(["Location", "Dialogue"])
 with main_tabs[0]:
     st.image(functionality.get_image_or_placeholder("placeholder_location_image.png", "yellow"), caption="Location")
-    st.chat_input("Enter text here")
+    
+st.chat_input("Enter text here") 
 
 with main_tabs[1]:
     dialogue_tabs = st.tabs(list(functionality.dialogue.speakers.keys()))
@@ -39,4 +40,5 @@ with main_tabs[1]:
             st.image(functionality.dialogue.speakers[speaker]["image"], caption=speaker)
             for text in functionality.dialogue.speakers[speaker]["dialogues"]:
                 st.write(text)
+
 
