83a01285be3eb0eb6e86b332f57e7cac643d6836
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 11:39:41 +0900

    Create fuctionality.py

---
+++ fuctionality.py
@@ -0,0 +1,40 @@
+class Player:
+    def __init__(self):
+        self.image = "placeholder_player_image.png"
+        self.stats = {"Health": 100, "Attack": 50, "Defense": 30}
+        self.inventory = []
+        self.equipment = {
+            "Head": None,
+            "Arms": None,
+            "Legs": None,
+            "Torso": None,
+            "Neck": None,
+            "Accessory1": None,
+            "Accessory2": None,
+        }
+        
+class Item:
+    def __init__(self, image, name, modifier, modifies, description):
+        self.image = image
+        self.name = name
+        self.modifier = modifier
+        self.modifies = modifies
+        self.description = description
+        
+class Dialogue:
+    def __init__(self):
+        self.speakers = {}
+
+    def add_speaker(self, name, image):
+        self.speakers[name] = {"image": image, "dialogues": []}
+
+    def add_dialogue(self, name, text):
+        if name in self.speakers:
+            self.speakers[name]["dialogues"].append(text)
+
+    def remove_speaker(self, name):
+        if name in self.speakers:
+            del self.speakers[name]
+
+player = Player()
+dialogue = Dialogue()
