90e61d552fd5a1ad505428b9c7308ce57ae49663
Karl Kablisk <karlkablisk@gmail.com>
2023-09-04 12:05:39 +0900

    a

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import streamlit as st
 import functionality
 from debug import debug_buttons
 
+#Image placeholder
+location_image = functionality.get_image_or_placeholder('yellow')
+st.image(location_image, caption="Location")
+
 st.title('Text-based Tabletop RPG')
 
 # Sidebar with tabs
-sidebar_tabs = st.sidebar.tabs(["Profile", "Settings"])
+sidebar_tabs = st.sidebar.tabs(["Profile", "Settings", "Debug"])
+
 with sidebar_tabs[0]:
-    st.image(functionality.player.image, caption="Player")
+    st.image(functionality.get_image_or_placeholder(functionality.player.image, 'blue'), caption="Player")
     st.write("Stats")
     for stat, value in functionality.player.stats.items():
         st.write(f"{stat}: {value}")
@@ -18,7 +23,7 @@ with sidebar_tabs[0]:
 
     st.write("Inventory")
     for item in functionality.player.inventory:
-        st.image(item.image)
+        st.image(functionality.get_image_or_placeholder(item.image, 'green'))
         st.write(f"Name: {item.name}")
         st.write(f"Modifier: {item.modifier}")
         st.write(f"Modifies: {item.modifies}")
@@ -27,20 +32,25 @@ with sidebar_tabs[0]:
 with sidebar_tabs[1]:
     st.file_uploader("Upload File")
 
+with sidebar_tabs[2]:
+    debug_buttons()
+
 # Main area with tabs
 main_tabs = st.tabs(["Location", "Dialogue"])
 with main_tabs[0]:
     st.image(functionality.get_image_or_placeholder("placeholder_location_image.png", "yellow"), caption="Location")
-    
-st.chat_input("Enter text here") 
+    st.chat_input("Enter text here")
 
 with main_tabs[1]:
-    dialogue_tabs = st.tabs(list(functionality.dialogue.speakers.keys()))
-    for speaker in functionality.dialogue.speakers:
-        with dialogue_tabs[speaker]:
-            st.image(functionality.dialogue.speakers[speaker]["image"], caption=speaker)
-            for text in functionality.dialogue.speakers[speaker]["dialogues"]:
-                st.write(text)
+    if functionality.dialogue.speakers:  # Checking if there are any speakers
+        dialogue_tabs = st.tabs(list(functionality.dialogue.speakers.keys()))
+        for speaker in functionality.dialogue.speakers:
+            with dialogue_tabs[speaker]:
+                st.image(functionality.get_image_or_placeholder(functionality.dialogue.speakers[speaker]["image"], 'red'), caption=speaker)
+                for text in functionality.dialogue.speakers[speaker]["dialogues"]:
+                    st.write(text)
+    else:
+        st.write("No dialogues available.")
 
 
 debug_buttons()
